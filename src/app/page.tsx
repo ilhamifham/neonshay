5dca1d94132b631e7072cdbd0961d19efb821a30
Ilham Ifham <ilhaamifham@gmail.com>
2025-09-23 10:00:04 +0530

    Added illustration

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,12 @@
+import Image from "next/image";
+
+import crochetIllustration from "@/assets/images/crochet.png";
+
 export default function Home() {
   return (
-    <section className="flex items-center justify-center min-h-full">
-      <h1 className="font-heading text-4xl flex items-center">
-        <svg className="mr-4 size-7 animate-spin text-black" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-          <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
-          <path fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
-        </svg>
-        Threading...
-      </h1>
+    <section className="flex flex-col items-center justify-center min-h-full p-4 gap-8">
+      <Image src={crochetIllustration} alt="" className="w-full max-w-[31.25rem] mx-auto" />
+      <h1 className="font-heading text-4xl animate-pulse">Crocheting...</h1>
     </section>
   );
 }
