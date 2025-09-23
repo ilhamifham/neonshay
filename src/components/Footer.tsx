2a65859cf27e5385fd278704f9e2c714744c1559
Ilham Ifham <ilhaamifham@gmail.com>
2025-09-23 14:53:56 +0530

    Updated the social icons

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,10 +4,11 @@ import SocialIconLink from "@/components/SocialIconLink";
 export default function Footer() {
   return (
     <footer className="px-8">
-      <div className="py-8 border-t border-t-neutral-200 flex gap-4 justify-center">
+      <div className="py-8 border-t border-t-neutral-200 flex flex-wrap gap-4 justify-center">
         {socialLinks.map((socialLink, index) => (
           <SocialIconLink key={index} link={socialLink.link} color={socialLink.color} icon={socialLink.path} />
         ))}
+        <p className="w-full text-center text-xs text-neutral-600">Copyright © 2025 Neonshay. All rights reserved.</p>
       </div>
     </footer>
   );
