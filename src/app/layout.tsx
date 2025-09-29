4bf6a8d7a6c9f542eb68a0b9eb9e018e57d23bba
Ilham Ifham <ilhaamifham@gmail.com>
2025-09-29 17:33:59 +0530

    Updated all

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,13 @@
 import type { Metadata } from "next";
 import "@/assets/globals.css";
 import { headingFont, bodyFont } from "@/assets/fonts";
-import Footer from "@/components/Footer";
 
 export const metadata: Metadata = {
-  title: { default: "Neonshay", template: "%s | Neonshay" },
-  description:
-    "A Sri Lankan brand that specializes in handcrafted crochet and fabric creations, including handmade bags, accessories, and home decor items, with an emphasis on elegance and unique design.",
+  title: {
+    default: "Neonshay",
+    template: "%s | Neonshay",
+  },
+  description: "A Sri Lankan brand that specializes in handcrafted crochet and fabric creations, including handmade bags, accessories, and home decor items, with an emphasis on elegance and unique design.",
 };
 
 export default function RootLayout({
@@ -16,12 +17,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={`${headingFont.variable} ${bodyFont.variable}`}>
-      <body>
-        <div className="h-full flex flex-col">
-          {children}
-          <Footer />
-        </div>
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,3 @@
-import Image from "next/image";
-
-import crochetIllustration from "@/assets/images/crochet.png";
-
 export default function Home() {
-  return (
-    <section className="flex-1 flex flex-col items-center justify-center p-4">
-      <Image src={crochetIllustration} alt="" className="w-full max-w-[31.25rem] mx-auto" priority />
-      <h1 className="font-heading text-heading text-4xl mt-8 mb-4">Crocheting...</h1>
-      <p>Site is under development.</p>
-    </section>
-  );
+  return <section>Hello, World.</section>;
 }
