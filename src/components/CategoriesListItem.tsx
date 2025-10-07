0ed8ff7d35eb366610e9e9c136dccabcacda3833
Ilham Ifham <ilhaamifham@gmail.com>
2025-10-07 23:05:56 +0530

    Updated some css

--- src/components/CategoriesListItem.tsx
+++ src/components/CategoriesListItem.tsx
@@ -1,4 +1,7 @@
+"use client";
+
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 type CategoriesListProps = {
   text: string;
@@ -7,14 +10,15 @@ type CategoriesListProps = {
 
 export default function CategoriesListItem({ text, emoji }: CategoriesListProps) {
   const urlLink = text === "all" ? "/" : `/${text}`;
+  const pathname = usePathname();
 
   return (
     <li>
-      <Link href={urlLink} className={"capitalize flex flex-col items-center gap-1 px-3 lg:flex-row group"}>
-        <div aria-hidden className={"text-[1.75rem] duration-300 group-hover:scale-110"}>
+      <Link href={urlLink} className={`capitalize flex flex-row items-center gap-2 pl-2 pr-2.5 rounded-sm ${pathname === urlLink ? "bg-subheading text-light" : "bg-light duration-300 transition-colors hover:bg-dark group"}`}>
+        <div aria-hidden className={"text-[1.75rem] duration-300 transition-transform group-hover:rotate-z-12"}>
           {emoji}
         </div>
-        <div className={"font-medium text-sm lg:text-base"}>{text}</div>
+        <div className={"font-medium text-sm"}>{text}</div>
       </Link>
     </li>
   );
